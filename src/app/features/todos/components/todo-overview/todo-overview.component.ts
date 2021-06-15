7dd9a4afa7be8895d111e9c723f64bceb41a06c9
Tommy Forsberg <tommy.forsberg@hotmail.com>
2021-06-15 10:05:38 +0200

    declare in ngoninit

--- src/app/features/todos/components/todo-overview/todo-overview.component.ts
+++ src/app/features/todos/components/todo-overview/todo-overview.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { merge,  Observable, ReplaySubject } from 'rxjs';
-import { mapTo, mergeMap, } from 'rxjs/operators';
+import { merge, Observable, ReplaySubject, throwError } from 'rxjs';
+import { catchError, mapTo, mergeMap, } from 'rxjs/operators';
 import { Todo } from '../../models/todo';
 import { User } from '../../models/user';
 import { TodoService } from '../../services/todo.service';
@@ -14,16 +14,31 @@ import { UserService } from '../../services/user.service';
 export class TodoOverviewComponent implements OnInit {
   constructor(private todoService: TodoService, private userService: UserService) { }
 
-  users: Observable<User[]> = this.userService.getUsers();
-  selectedUserId: ReplaySubject<number> = new ReplaySubject<number>();
-  userTodos: Observable<Todo[]> = this.selectedUserId.pipe(mergeMap(id => this.todoService.getTodos(id)));
-  spin: Observable<boolean> = merge(...[this.selectedUserId.pipe(
-  mapTo(true)),
-  this.userTodos.pipe(mapTo(false))]);
+  users!: Observable<User[]>
+  selectedUserId!: ReplaySubject<number>;
+  userTodos!: Observable<Todo[]>;
+  loading!: Observable<boolean>;
 
   ngOnInit(): void {
+    this.users = this.userService.getUsers();
+    this.selectedUserId = new ReplaySubject<number>();
+    this.userTodos = this.selectedUserId.pipe(mergeMap(id => this.todoService.getTodos(id)
+      .pipe(
+        catchError((err) => {
+          //error handling should be done here
+          console.error(err);
+          return throwError(err);
+        }))));
 
+    this.loading = merge(...[this.selectedUserId.pipe(
+      mapTo(true)),
+    this.userTodos.pipe(mapTo(false))]);
   }
 }
 
+catchError((err) => {
+  console.log('error caught in service')
+  console.error(err);
 
+  return throwError(err);
+})
